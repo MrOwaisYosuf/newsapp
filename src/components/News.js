bb0e5f937dfe16e97b1d01309ea7594fd52aba68
Owais Yosuf <johnowais@gmail.com>
2025-04-21 15:23:27 +0530

    Fixed NavBar

--- src/components/News.js
+++ src/components/News.js
@@ -36,8 +36,8 @@ const News = (props) => {
     }, []);
 
     const fetchMoreData = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`
         setPage(page + 1)
-        let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
         let data = await fetch(url)
         let parsedData = await data.json()
         setArticles(articles.concat(parsedData.articles));
@@ -46,7 +46,7 @@ const News = (props) => {
 
     return (
         <>
-            <h2 className='text-center my-4'>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h2>
+            <h2 className='text-center' style={{ marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h2>
             {loading && <Spinner />}
             <InfiniteScroll
                 dataLength={articles.length}
